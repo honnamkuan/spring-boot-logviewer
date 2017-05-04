7d6a8010afdb907f09005de081143cb83cf7ee8a
Hon Nam Kuan <honnamkuan@gmail.com>
2017-05-04 22:59:47 +0800

    Initial Commit

---
+++ src/main/java/com/hnkuan/SpringBootLogviewerApplication.java
@@ -0,0 +1,12 @@
+package com.hnkuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLogviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLogviewerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hnkuan/SpringBootLogviewerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hnkuan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootLogviewerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
